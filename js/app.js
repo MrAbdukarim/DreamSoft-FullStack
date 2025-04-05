fcf78870708cd601750524eb86d1c3ea8d0d0af5
MrAbdukarim <abdukarimqarshiyev1@gmail.com>
2025-04-05 19:48:49 +0500

    Add changes

--- js/app.js
+++ js/app.js
@@ -66,3 +66,28 @@ each('.filter-links li a', function (el) {
         filterLinks(el);
     });
 });
+
+// Counter Number animation
+function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        obj.innerHTML = Math.floor(progress * (end - start) + start);
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+}
+
+const obj = document.getElementById("value");
+const obj2 = document.getElementById("value2");
+const obj3 = document.getElementById("value3");
+const obj4 = document.getElementById("value4");
+const obj5 = document.getElementById("value5");
+animateValue(obj, 0, 10, 3000);
+animateValue(obj2, 0, 2, 2000);
+animateValue(obj3, 0, 40, 3000);
+animateValue(obj4, 0, 12, 3000);
+animateValue(obj5, 0, 160, 3000);
